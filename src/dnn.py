1f7d7c9f660812c0a81cb32c8563b185d8847a11
mindia <mindia@traceventures.es>
2023-05-06 00:28:56 +0200

    Added Docstring

--- src/dnn.py
+++ src/dnn.py
@@ -2,7 +2,13 @@ import torch
 from torch import nn
 
 class DNN(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    '''DNN Class for the snake game.'''
+    def __init__(self, input_size: int, output_size: int, hidden_size: int):
+        '''Initialize the DNN.
+        Args: input_size (int): Input size
+              output_size (int): Output size
+              hidden_size (int): Hidden size
+        '''
         super(DNN, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
@@ -10,10 +16,15 @@ class DNN(nn.Module):
         self._init_layers()
 
     def _init_layers(self):
+        '''Initialize the layers.'''
         self.nn1 = nn.Linear(self.input_size, self.hidden_size)
         self.nn2 = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, x):
+    def forward(self, x: torch.tensor):
+        '''Compute the forward pass.
+        Args: x (torch.tensor): Input tensor
+        Returns: torch.tensor: Output tensor
+        '''
         x = torch.relu(self.nn1(x))
         x = self.nn2(x)
         return torch.softmax(x, dim=1)
