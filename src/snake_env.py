109fcd41db9f0d11d5cdeaeeabcd82d22b0c17c2
mindia <mindia@traceventures.es>
2023-05-06 00:33:04 +0200

    Added Docstring

--- src/snake_env.py
+++ src/snake_env.py
@@ -4,15 +4,17 @@ import numpy as np
 from src.snake_game import SnakeGame
 
 class SnakeEnv(gym.Env):
-    
+    '''Snake Environment.'''
     ACTIONS_DICT = {
         0: "RIGHT",
         1: "LEFT",
         2: "UP",
         3: "DOWN"
     }
-
     def __init__(self, board_size = 350):
+        '''Initialize the Snake Environment.
+        Args: board_size (int): Size of the board
+        '''
         super(SnakeEnv, self).__init__()
         self.board_size = board_size
         self.score = 0
@@ -22,6 +24,7 @@ class SnakeEnv(gym.Env):
         
 
     def _update_observation_space(self):
+        '''Update the observation space.'''
         snake_position, snake_body, food_position = self.game.get_elements_space()
         self.observation_space = -np.ones((self.board_size, self.board_size), dtype=np.int8)
         for snake_point in snake_body[1:]:
@@ -29,7 +32,12 @@ class SnakeEnv(gym.Env):
         self.observation_space[snake_position[0], snake_position[1]] = 1
         self.observation_space[food_position[0], food_position[1]] = 2
 
-    def _compute_reward(self, score, done):
+    def _compute_reward(self, score: int, done: bool) -> float():
+        '''Compute the reward.
+        Args: score (int): Score
+              done (bool): Done
+        Returns: float: Reward
+        '''
         if done:
             return -20
         elif score > self.score:
@@ -39,15 +47,21 @@ class SnakeEnv(gym.Env):
             return 0.1
 
     def reset(self):
+        '''Reset the environment.'''
         self.game.reset()
         self.score = 0
         self._update_observation_space()
         return self.observation_space
 
-    def step(self, action):
+    def step(self, action: int)-> tuple[np.array, float, bool, dict, bool]:
+        '''Take a step in the environment.
+        Args: action (int): Action
+        Returns: tuple[np.array, float, bool, dict, bool]: Observation, reward, done, info, done
+        '''
         states, score, _, _, done = self.game.step(self.ACTIONS_DICT[action])
         snake_position, _, food_position = self.game.get_elements_space()
         return self.observation_space, self._compute_reward(score, done), _, _, done
 
     def render(self):
+        '''Render the environment.'''
         self.game.render()
