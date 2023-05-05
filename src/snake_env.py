a2f2abfcf314b4256d97e662fa4b23771511428b
mindia <mindia@traceventures.es>
2023-05-05 23:01:21 +0200

    Reduced Board Size

--- src/snake_env.py
+++ src/snake_env.py
@@ -36,7 +36,7 @@ class SnakeEnv(gym.Env):
             self.score = score
             return 10
         else:
-            return 1
+            return 0
 
     def reset(self):
         self.game.reset()
