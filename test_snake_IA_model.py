622f5291444fe6479ff6986f639e9655f4ad2bcc
mindia <mindia@traceventures.es>
2023-05-07 00:46:15 +0200

    Changed DNN call

--- test_snake_IA_model.py
+++ test_snake_IA_model.py
@@ -9,7 +9,7 @@ BOARD_SIZE = 200
 def main(model_name: str):
     '''Main function.'''
     env = SnakeEnv(board_size=BOARD_SIZE)
-    dnn =  DNN(BOARD_SIZE**2, 4, BOARD_SIZE)
+    dnn =  DNN(4, BOARD_SIZE)
     trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
     trainer.test(games=25)
 
