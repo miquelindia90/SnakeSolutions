29d59a2954a969ef7367d105c6aacd53da094a1b
mindia <mindia@traceventures.es>
2023-05-07 01:05:48 +0200

    Changed Board Size

--- test_snake_IA_model.py
+++ test_snake_IA_model.py
@@ -4,12 +4,13 @@ from src.snake_env import SnakeEnv
 from src.dnn import DNN
 from src.rl_trainer import RlTrainer
 
-BOARD_SIZE = 200
+BOARD_SIZE = 400
+HIDDEN_SIZE = 100
 
 def main(model_name: str):
     '''Main function.'''
     env = SnakeEnv(board_size=BOARD_SIZE)
-    dnn =  DNN(4, BOARD_SIZE)
+    dnn =  DNN(4, HIDDEN_SIZE)
     trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
     trainer.test(games=25)
 
