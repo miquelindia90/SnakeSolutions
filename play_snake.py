3c83ceff997fc2da63ff557304ad3e62b0de2665
mindia <mindia@traceventures.es>
2023-05-07 01:23:00 +0200

    Set Display

--- play_snake.py
+++ play_snake.py
@@ -2,7 +2,7 @@ from src.snake_game import SnakeGame
 
 def main():
     '''Main function.'''
-    game = SnakeGame()
+    game = SnakeGame(display=True)
     game.start()
 
 if __name__ == '__main__':
