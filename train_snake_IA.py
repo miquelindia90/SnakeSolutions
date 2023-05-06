8613ba005d0ca38ebfe797520e6da5fd08bd6348
mindia <mindia@traceventures.es>
2023-05-06 17:04:49 +0200

    Added Model Name as Input

--- train_snake_IA.py
+++ train_snake_IA.py
@@ -1,15 +1,17 @@
+import sys
+
 from src.snake_env import SnakeEnv
 from src.dnn import DNN
 from src.rl_trainer import RlTrainer
 
 BOARD_SIZE = 200
 
-def main():
+def main(model_name: str):
     '''Main function.'''
     env = SnakeEnv(board_size=BOARD_SIZE)
     dnn =  DNN(BOARD_SIZE**2, 4, BOARD_SIZE)
-    trainer = RlTrainer(env, dnn)
+    trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
     trainer.train()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
