4ee52a55f37deb8a6f1bb274566638f1a62db45c
mindia <mindia@traceventures.es>
2023-05-06 23:50:46 +0200

    Changes number of testing games

--- test_snake_IA_model.py
+++ test_snake_IA_model.py
@@ -11,7 +11,7 @@ def main(model_name: str):
     env = SnakeEnv(board_size=BOARD_SIZE)
     dnn =  DNN(BOARD_SIZE**2, 4, BOARD_SIZE)
     trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
-    trainer.test(games=10)
+    trainer.test(games=25)
 
 if __name__ == '__main__':
     main(sys.argv[1])
