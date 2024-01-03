bb670f8a679f271d29d5d4c5c0bfc9c6e9b8991d
mindia <mindia@traceventures.es>
2024-01-03 23:47:10 +0100

    Added Script to Evaluate Models

---
+++ evaluate_snake_IA_model.py
@@ -0,0 +1,18 @@
+import sys
+
+from src.snake_env import SnakeEnv
+from src.dnn import DNN
+from src.rl_trainer import RlTrainer
+
+BOARD_SIZE = 150
+HIDDEN_SIZE = 100
+
+def main(model_name: str) -> None:
+    '''Main function.'''
+    env = SnakeEnv(board_size=BOARD_SIZE, display=False)
+    dnn =  DNN(4, HIDDEN_SIZE)
+    trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
+    trainer.test(games=1000, display=False)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
