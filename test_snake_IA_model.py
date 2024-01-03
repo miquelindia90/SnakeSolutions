75c727f0de97e84cf4e55067f59e41d1315ef610
mindia <mindia@traceventures.es>
2024-01-03 23:38:05 +0100

    Adapted Methods for new Agent/Env Changes

--- test_snake_IA_model.py
+++ test_snake_IA_model.py
@@ -7,12 +7,12 @@ from src.rl_trainer import RlTrainer
 BOARD_SIZE = 150
 HIDDEN_SIZE = 100
 
-def main(model_name: str):
+def main(model_name: str) -> None:
     '''Main function.'''
     env = SnakeEnv(board_size=BOARD_SIZE, display=True)
     dnn =  DNN(4, HIDDEN_SIZE)
     trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
-    trainer.test(games=25)
+    trainer.test(games=25, display=True)
 
 if __name__ == '__main__':
     main(sys.argv[1])
