c9024f0dd6b9775620b1f7ecaf8dffd03784e9b6
mindia <mindia@traceventures.es>
2024-01-04 12:07:14 +0100

    Used argparse to declare variables

--- src/train_snake_IA.py
+++ src/train_snake_IA.py
@@ -1,20 +1,38 @@
 import sys
+import argparse
 
 from snake_env import SnakeEnv
 from dnn import DNN
 from rl_trainer import RlTrainer
 
-BOARD_SIZE = 150
-HIDDEN_SIZE = 100
 
 
-def main(model_name: str):
+def main(parameters: argparse.Namespace) -> None:
     """Main function."""
-    env = SnakeEnv(board_size=BOARD_SIZE)
-    dnn = DNN(4, HIDDEN_SIZE)
-    trainer = RlTrainer(env=env, dnn=dnn, model_name=model_name)
+    env = SnakeEnv(board_size=parameters.board_size)
+    dnn = DNN(4, parameters.hidden_size)
+    trainer = RlTrainer(env=env, dnn=dnn, parameters=parameters)
     trainer.train()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_name", type=str, help="Name of the model to be trained")
+    parser.add_argument("--board_size", type=int, default=150, help="Size of the board")
+
+    # DNN Configuration
+    parser.add_argument("--hidden_size", type=int, default=100, help="Size of the hidden layer")
+
+    # Training Configuration
+    parser.add_argument("--learning_rate", type=float, default=0.0001, help="Learning rate")
+    parser.add_argument("--max_episodes", type=int, default=1_000_000, help="Maximum number of episodes")
+    parser.add_argument("--batch_size", type=int, default=20, help="Batch size")
+    parser.add_argument("--gamma", type=float, default=0.95, help="Discount factor")
+    parser.add_argument("--initial_epsilon", type=float, default=0.5, help="Initial epsilon value")
+    parser.add_argument("--target_epsilon", type=float, default=0.01, help="Maximum epsilon value")
+    parser.add_argument("--epsilon_converge_ratio", type=float, default=1 / 3, help="Ratio for epsilon convergence")
+    parser.add_argument("--buffer_size", type=int, default=5000, help="Size of the replay buffer")
+
+    parameters = parser.parse_args()
+    main(parameters)
